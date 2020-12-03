5849624f635a3fff832f186d8589953531d7ca25
sanchit313 <sanchit313>
2020-12-03 00:47:19 +0000

    Adding a basic Sanity test to verify files are successfully loaded

--- MOOC Data Management and Exploratory Analysis/tests/1.R
+++ MOOC Data Management and Exploratory Analysis/tests/1.R
@@ -1,3 +1,10 @@
-# Example Unit Testing Script
-context("Example tests")
-expect_equal(1, 1)
+#  Unit Testing Script
+context("Basic checks")
+
+# Test data
+
+#test video stats file from  analysis b.r
+test_that("Correct columns in 1 of the video stats file", {
+  expect_equal(ncol(Extracted.video3), 6)
+})
+
